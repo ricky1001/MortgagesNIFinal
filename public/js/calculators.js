761f6729be26a40653e05bb461d62d8c4994b0a3
Richard Aston <ricky1001@hotmail.co.uk>
2019-02-28 09:54:01 +0000

    add calculator

--- public/js/calculators.js
+++ public/js/calculators.js
@@ -2,6 +2,7 @@ var show = document.querySelector(".addSecondApp");
 
 show.addEventListener("click", function(){
   $( "#secondAppHidden.secondAppInput" ).toggleClass( "secondAppInputDisplay" );
+  $(".addSecondApp").toggleClass("secondAppInput");
 });
 
 
@@ -11,23 +12,44 @@ var btn = document.querySelector(".totalBorrow");
 
 btn.addEventListener("click", function(){
 
+   var secondIncome = "0";
+   var secondBonus = "0";
+   var firstBonus = "0";
+
   var firstIncome = $('#firstIncome').val();
-  var firstBonus = $("#firstBonus").val();
-  var secondIncome = $("#secondIncome").val();
-  var secondBonus = $("#secondBonus").val();
+      fB = $("#firstBonus").val();
+      sI = $("#secondIncome").val();
+      sB = $("#secondBonus").val();
+
+      if(sI !== ""){
+        secondIncome = sI;
+        }
+
+      if(sB !== ""){
+        secondBonus = sB;
+      }
+
+      if(fB !== ""){
+        firstBonus = fB;
+      }
 
   firstIncome =  parseInt(firstIncome, 10);
   firstBonus =  parseInt(firstBonus, 10);
-  secondIncome = parseInt(secondIncome, 10);
-  secondBonus = parseInt(secondBonus, 10);
-
+  secondIncome =  parseInt(secondIncome, 10);
+  secondBonus =  parseInt(secondBonus, 10);
 
 maxLoan = ((firstIncome + firstBonus + secondIncome + secondBonus) * 5);
 
-$( "#maxBorrow.youCouldBorrow" ).toggleClass( "youCouldBorrowShow" );
+//$( "#maxBorrow.youCouldBorrow" ).toggleClass( "youCouldBorrowShow" );
+var showMax = document.getElementById("maxBorrow");
+showMax.className = "youCouldBorrowShow";
+
+
 
 var sp = document.getElementById("totLoan");
-sp.innerHTML = "£" + maxLoan;
+sp.innerHTML = "£" + maxLoan.toLocaleString('en');
+
+
 
 
 
